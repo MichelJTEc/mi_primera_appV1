5460b16f6b0cf974de7d289c159baa0d24590097
MichelJTEc <mjtapia@estud.tesa.edu.ec>
2026-03-03 22:13:59 -0500

    Con Camara

---
+++ src/stores/user.ts
@@ -0,0 +1,43 @@
+import { defineStore } from "pinia";
+import { ref } from "vue";
+import axiosRiksiri from "@/axios/axiosRiksiri";    
+
+export const useUserStore = defineStore('user', () => {
+
+    const token = ref(localStorage.getItem('token') || null);
+    const login = ref({
+        username: null,
+        password: null  
+    })
+
+    const registro = ref({
+        usuario: null,
+        email: null,
+        password: null,
+    })
+
+    function $registro(){
+        return axiosRiksiri.post('register', registro.value).then( res => {
+            $setLogin(res.data);
+            return res.data;
+        })
+    }
+    
+    function $login(){
+        return axiosRiksiri.post('login', login.value).then( res => {
+            $setLogin(res.data);
+            return res.data;
+        });
+    }
+
+    function $setLogin(data: any | null){
+        token.value = data?.token || null;
+        if(token.value) {
+            localStorage.setItem('token', token.value);
+        } else {
+            localStorage.removeItem('token');
+        }
+    }
+
+    return { login, $login, token, $setLogin, registro, $registro }
+});
